eebb0a86e4d27b96ac45733d95b551f36be13002
Pedro Benedetti <pbenedetti@itba.edu.ar>
2022-12-01 15:07:08 -0300

    Update AUT_and_REY.py

--- AUT_and_REY.py
+++ AUT_and_REY.py
@@ -1,6 +1,9 @@
-# UPDATED 28/11/2022
+# UPDATED 01/12/2022
 # AUT and REY protocol.
 
+####
+##Import libraries
+####
 import pygame
 from pygame.locals import *
 import sys
@@ -9,8 +12,9 @@ import logging
 import math
 import pandas as pd    
 
-
-
+####
+## Initiate Logger file
+####
 logging.basicConfig(filename = "logger_AUT_and_REY.txt", level = logging.DEBUG)
 time_txt = datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
 mess0 = "\n Welcome to AUT and REY protocol"
@@ -18,89 +22,104 @@ logging.debug(mess0)
 mess = (time_txt + ": Initializing protocol...")
 logging.debug(mess)
 
-# try:
-#     from matlab_parallel_com import *
-#     # imports script where MATLAB fuctions are defined. MATLAB engine will be started.
-# except:
-#     # throws an exception and message if some error ocurred.
-#     print("Can't import 'matlab_parallel_com'")
-#     time_txt = datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
-#     mess = (time_txt + ": Can't import 'matlab_parallel_com'")
-#     logging.error(mess)
+####
+## Imports script where MATLAB fuctions are defined. MATLAB engine will be started.
+####
+try:
+    from matlab_parallel_com import *
+except:
+    # throws an exception and message if some error ocurred.
+    print("Can't import 'matlab_parallel_com'")
+    time_txt = datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
+    mess = (time_txt + ": Can't import 'matlab_parallel_com'")
+    logging.error(mess)
 
-#     port = 'C020'
+    port = 'C020'
 
-pygame.init()
-# # Initiate pygame
-# try:
-#     send_mark_biosemi(20, port)
-#     # A mark (25) is sent to the EEG indicating that the code has started.
-# except:
-#     print("Can't execute 'send_mark_biosemi' properly")
-#     time_txt = datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
-#     mess = (time_txt + ": Can't execute 'send_mark_biosemi' properly")
-#     logging.error(mess)
+pygame.init() #Initiate pygame
+
+####
+# Send Mark indicating the protocol has started.
+####
+try:
+    send_mark_biosemi(20, port)
+except:
+    print("Can't execute 'send_mark_biosemi' properly")
+    time_txt = datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
+    mess = (time_txt + ": Can't execute 'send_mark_biosemi' properly")
+    logging.error(mess)
 
 
 window = pygame.display.set_mode((0, 0),pygame.FULLSCREEN)
-
-newsplane = pygame.image.load('C:/Users/pbenedetti/Documents/Doctorado/Biosemi/Scripts/newsplane.png')
-clip = pygame.image.load("C:/Users/pbenedetti/Documents/Doctorado/Biosemi/Scripts/clip.png")
 window.fill((238, 238, 238))
-# Fill the screen with white color
-
 X,Y = window.get_size()
 pygame.display.update()
-pag_count=0
-code = ""
-input_rect = pygame.Rect(0,0,500,500)
-active = False
-title_font = pygame.font.Font(None,60)
-input_font = pygame.font.Font(None,32)
-time_font = pygame.font.Font(None, 100)
-input_text = []
-current_use = ""
-N_uses = 0
-soundObj = pygame.mixer.Sound('C:/Users/pbenedetti/Documents/Doctorado/Biosemi/Scripts/010762485_prev_1.wav')
-output = []
+
+newsplane = pygame.image.load('C:/Users/pbenedetti/Documents/Doctorado/Biosemi/Scripts/newsplane.png') #Image available on https://drive.google.com/file/d/1khTMM9Fs5iIt-V4cTV5PTUFyKDczdU0g/view?usp=share_link
+clip = pygame.image.load("C:/Users/pbenedetti/Documents/Doctorado/Biosemi/Scripts/clip.png") #Image available on https://drive.google.com/file/d/1RboI8fX5lwLC24OwUrFYIdizChEHX6wy/view?usp=share_link
+soundObj = pygame.mixer.Sound('C:/Users/pbenedetti/Documents/Doctorado/Biosemi/Scripts/bell.wav') #Sound available on https://drive.google.com/file/d/1ZEsz7WcAmguzk7TIWu652IBkieHrBXdS/view?usp=share_link
+
+pag_count=0 #Indicates in wich page of the protocol we are located
+code = "" #Input text variable for participant code
+input_rect = pygame.Rect(0,0,500,500) #Rectangle for input text
+title_font = pygame.font.Font(None,60) #Title font
+input_font = pygame.font.Font(None,32) #Body text font
+time_font = pygame.font.Font(None, 100) #Time counter font
+input_text = [] #List with uses saved
+current_use = "" #Input text variable for currently wrinting use
+N_uses = 0 #Number of uses saved on 'input_text'
+
+output = [] #Output array
 dt= "Date: " + str(datetime.datetime.now())
 header = [dt]
-print(output)
 
-while True:
-    if pag_count == 0:
+while True: #permanently running
+    if pag_count == 0: #PARTICIPANT CODE INPUT
         window.fill((238, 238, 238))
         text_surface = title_font.render("Codigo de participante: ", True, (234, 64, 142))
-        text_surface2 = input_font.render(code, True, (74,64,103))
         window.blit(text_surface, (X/2-text_surface.get_width()/2, Y/2-100))
+        text_surface2 = input_font.render(code, True, (74,64,103))
         window.blit(text_surface2, (X/2-text_surface2.get_width()/2,Y/2))
+        text_surface3 = input_font.render("Presioná 'Enter' para avanzar.", True, (74,64,103))
+        window.blit(text_surface3, (X/2-text_surface3.get_width()/2,Y-100))
 
-    if pag_count == 1:
+    if pag_count == 1: #PART 1 TITLE (AUT)
         window.fill((238, 238, 238))
         text_surface = title_font.render("PARTE 1", True, (234, 64, 142))
         window.blit(text_surface, (X/2-text_surface.get_width()/2, Y/2))
 
-    if pag_count == 2:
+        text_surface8 = input_font.render("Presioná 'Enter' para avanzar.", True, (74,64,103))
+        window.blit(text_surface8, (X/2-text_surface3.get_width()/2,Y-100))
+
+    if pag_count == 2: #INSTRUCTIONS FOR AUT
         window.fill((238, 238, 238))
+
         text_surface = title_font.render("INSTRUCCIONES:", True, (234, 64, 142))
-        text_surface2 = input_font.render("El objetivo es escribir todos los usos alternativos que se te ocurran para un objeto.", True, (74,64,103))
-        text_surface3 = input_font.render("Después de iniciar, escribí tus propuestas y presioná 'Enter' al terminar cada una.", True, (74,64,103))
-        text_surface4 = input_font.render("Usá tu imaginación para generar tus propuestas. No hay límites.", True, (74,64,103))
-        text_surface5 = input_font.render("Tu única limitación es el tiempo, vas a disponer de 5 minutos.", True, (74,64,103))
-        text_surface6 = input_font.render("Te damos un ejemplo: ¿Que usos le darías a un papel de diario?", True, (74,64,103))
-        text_surface7 = input_font.render("Una respuesta podría ser 'Hacer un avión de papel'", True, (74,64,103))
-
         window.blit(text_surface, (X/2-text_surface.get_width()/2, 50))
-        window.blit(text_surface2, (X/2-text_surface2.get_width()/2,Y/2-150))
-        window.blit(text_surface3, (X/2-text_surface3.get_width()/2,Y/2-125))
-        window.blit(text_surface4, (X/2-text_surface4.get_width()/2,Y/2-75))
-        window.blit(text_surface5, (X/2-text_surface5.get_width()/2,Y/2-50))
-        window.blit(text_surface6, (X/2-text_surface6.get_width()/2,Y/2+0))
-        window.blit(text_surface7, (X/2-text_surface7.get_width()/2,Y/2+25))
-        window.blit(newsplane, (X/2-newsplane.get_width()/2,Y/2+100))
         
+        text_surface2 = input_font.render("El objetivo es escribir todos los usos alternativos que se te ocurran para un objeto.", True, (74,64,103))
+        window.blit(text_surface2, (X/2-text_surface2.get_width()/2,Y/2-150))
+        
+        text_surface3 = input_font.render("Después de iniciar, escribí tus propuestas y presioná 'Enter' al terminar cada una.", True, (74,64,103))
+        window.blit(text_surface3, (X/2-text_surface3.get_width()/2,Y/2-125))
+        
+        text_surface4 = input_font.render("Usá tu imaginación para generar tus propuestas. No hay límites.", True, (74,64,103))
+        window.blit(text_surface4, (X/2-text_surface4.get_width()/2,Y/2-75))
+        
+        text_surface5 = input_font.render("Tu única limitación es el tiempo, vas a disponer de 5 minutos.", True, (74,64,103))
+        window.blit(text_surface5, (X/2-text_surface5.get_width()/2,Y/2-50))
+        
+        text_surface6 = input_font.render("Te damos un ejemplo: ¿Que usos le darías a un papel de diario?", True, (74,64,103))
+        window.blit(text_surface6, (X/2-text_surface6.get_width()/2,Y/2+0))
+        
+        text_surface7 = input_font.render("Una respuesta podría ser 'Hacer un avión de papel'", True, (74,64,103))
+        window.blit(text_surface7, (X/2-text_surface7.get_width()/2,Y/2+25))
+        
+        window.blit(newsplane, (X/2-newsplane.get_width()/2,Y/2+100))
 
-    if pag_count == 3:
+
+
+    if pag_count == 3: #AUT
         window.fill((238, 238, 238))
         time_now = datetime.datetime.now()
         
@@ -154,24 +173,31 @@ while True:
                 pag_count = pag_count + 1
                 
     
-    if pag_count == 4:
+    if pag_count == 4: #TILE PART 2 (REY FIGURE TEST)
         window.fill((238, 238, 238))
+
         text_surface = input_font.render("Se acabó el tiempo. Pasamos a la siguiente parte:", True, (74,64,103))
+        window.blit(text_surface, (X/2-text_surface.get_width()/2, 50))
+
         text_surface2 = title_font.render("PARTE 2", True, (234, 64, 142))
-        window.blit(text_surface, (X/2-text_surface.get_width()/2, 50))
         window.blit(text_surface2, (X/2-text_surface2.get_width()/2,Y/2))
 
-    if pag_count == 5:
+        text_surface8 = input_font.render("Presioná 'Enter' para avanzar.", True, (74,64,103))
+        window.blit(text_surface8, (X/2-text_surface3.get_width()/2,Y-100))
+    
+    if pag_count == 5: #INSTRUCTIONS REY FIGURE TEST
         window.fill((238, 238, 238))
-        text_surface = title_font.render("INSTRUCCIONES:", True, (234, 64, 142))
-        text_surface2 = input_font.render("Por último, tenés que dibujar la misma figura que hace dos días.", True, (74,64,103))
-        text_surface3 = input_font.render("Tenés un tiempo total de 3 minutos para hacerlo. Presioná 'Enter' para comenzar.", True, (74,64,103))
 
+        text_surface = title_font.render("INSTRUCCIONES:", True, (234, 64, 142))
         window.blit(text_surface, (X/2-text_surface.get_width()/2, 50))
+
+        text_surface2 = input_font.render("Por último, tenés que dibujar la misma figura que hace dos días.", True, (74,64,103))
         window.blit(text_surface2, (X/2-text_surface2.get_width()/2,Y/2))
+
+        text_surface3 = input_font.render("Tenés un tiempo total de 3 minutos para hacerlo. Presioná 'Enter' para comenzar.", True, (74,64,103))
         window.blit(text_surface3, (X/2-text_surface3.get_width()/2,Y/2+50))
 
-    if pag_count == 6:
+    if pag_count == 6: #REY FIGURE TEST TIME
         window.fill((238, 238, 238))
         time_now = datetime.datetime.now()
         if started_REY == 0:
@@ -205,12 +231,15 @@ while True:
                 soundObj.play()
                 pag_count = pag_count + 1
 
-    if pag_count == 7:
+    if pag_count == 7: #THANKS
         window.fill((238, 238, 238))
-        text_surface = title_font.render("MUCHAS GRACIAS POR PARTICIPAR!", True, (234, 64, 142))
+        text_surface = title_font.render("Muchas gracias por participar!", True, (234, 64, 142))
         window.blit(text_surface, (X/2-text_surface.get_width()/2, Y/2))
+
+        text_surface8 = input_font.render("Presioná 'Enter' para finalizar.", True, (74,64,103))
+        window.blit(text_surface8, (X/2-text_surface3.get_width()/2,Y-100))
     
-    if pag_count == 8:
+    if pag_count == 8: #SAVING
         print(output)
         output = output
         filename = code + '_AUTandREY.csv'
@@ -219,42 +248,39 @@ while True:
         data.to_csv(filename, index=False)
         sys.exit()
 
-
-
     pygame.display.update()
 
+##################
+## Inputs handling
+##################
     for event in pygame.event.get():
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            if input_rect.collidepoint(event.pos):
-                active = True
-            else: 
-                active = False
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_RIGHT:
-                pag_count = pag_count + 1
+                pag_count = pag_count # Nothing happens in this version
             elif event.key == pygame.K_ESCAPE:
                 print(input_text)
-                # the user can quit the protocol by pressing Q key
-                # try:
-                # # one last mark is sent indicating that the protocol is over
-                #     send_mark_biosemi(250, port)
-                #     time_txt = datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
-                #     mess = (time_txt + ": Ending protocol..."+"\n")
-                #     logging.debug(mess)
-                # except:
-                #     print("Can't execute 'send_mark_biosemi' properly")
-                #     time_txt = datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
-                #     mess = (time_txt + ": Can't execute 'send_mark_biosemi' properly")
-                #     logging.error(mess)
-                # close_eng()
+                try:
+                    send_mark_biosemi(250, port)
+                    time_txt = datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
+                    mess = (time_txt + ": Ending protocol..."+"\n")
+                    logging.debug(mess)
+                except:
+                    print("Can't execute 'send_mark_biosemi' properly")
+                    time_txt = datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
+                    mess = (time_txt + ": Can't execute 'send_mark_biosemi' properly")
+                    logging.error(mess)
+                
+                close_eng()
                 pygame.quit()
                 sys.exit()
 
-            elif event.key == pygame.K_BACKSPACE:
-                if pag_count==3:
+            elif event.key == pygame.K_BACKSPACE: #If user needs to delete input text
+                if pag_count == 3:
                     current_use = current_use[0:-1]
+                if pag_count == 1:
+                    code = code [0:-1]
 
-            elif event.key == pygame.K_RETURN:
+            elif event.key == pygame.K_RETURN: # 'Enter' Button. Advance in pages and saves uses.
                 if pag_count == 7:
                     pag_count = pag_count + 1
 
@@ -270,8 +296,6 @@ while True:
                     if len(current_use)>0:
                         input_text.append(current_use)
                         output.append(current_use)
-                        print(output)
-
                         current_use = ""
                         N_uses = N_uses + 1
 
@@ -285,32 +309,26 @@ while True:
     
                 if pag_count == 0:
                     output.append("Participant: " + code)
-                    print(output)
-
                     pag_count = pag_count + 1
                 
-
-
-
-            else:
+            else: #If button pressed is not 'Enter', 'Backspace' or Right arrow. Builds input text.
                 if pag_count == 0:
                     code += event.unicode
                 if pag_count == 3 and started_AUT == 1:
                     current_use += event.unicode
                 
 
-        if event.type == pygame.QUIT:
-            # try:
-            #     # one last mark is sent indicating that the protocol is over
-            #     send_mark_biosemi(250, port)
-            #     time_txt=datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
-            #     mess = (time_txt + ": Ending protocol..."+"\n")
-            # except:
-            #     print("Can't execute 'send_mark_biosemi' properly")
-            #     time_txt = datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
-            #     mess = (time_txt + ": Can't execute 'send_mark_biosemi' properly")
-            #     logging.error(mess)
-            # logging.debug(mess)
-            #close_eng()
+        if event.type == pygame.QUIT: #If user quits.
+            try:
+                send_mark_biosemi(250, port)
+                time_txt=datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
+                mess = (time_txt + ": Ending protocol..."+"\n")
+            except:
+                print("Can't execute 'send_mark_biosemi' properly")
+                time_txt = datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
+                mess = (time_txt + ": Can't execute 'send_mark_biosemi' properly")
+                logging.error(mess)
+            logging.debug(mess)
+            close_eng()
             pygame.quit()
             sys.exit()
