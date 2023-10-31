85add700fe723d5ca4e7aac5839324b3460d61c0
Pedro Benedetti <pbenedetti@itba.edu.ar>
2023-10-31 11:56:53 -0300

    Update impparallel.py

--- impparallel.py
+++ impparallel.py
@@ -1,8 +1,23 @@
 import sys
-sys.path.append('C:/Users/pedro/src')
+sys.path.append('D:/Pedro_Benedetti/lib')
 from psychopy import parallel
+import datetime
 
-adress = 0xC020
 
-parallel.setPortAddress(adress)
-parallel.setData(20)
+
+adress = 0xCFE8
+
+def send_mark_biosemi(mark, adress):
+    parallel.setPortAddress(adress)
+
+    time_start = datetime.datetime.now()
+    time_now = datetime.datetime.now()
+
+    while True:
+        parallel.setData(mark)
+        time_now = datetime.datetime.now()
+        print('B')
+        if (time_now - time_start).total_seconds() > 0.0015:
+            print((time_now - time_start).total_seconds())
+            parallel.setData(0)
+            break
