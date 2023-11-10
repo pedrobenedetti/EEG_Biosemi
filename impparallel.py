9052058591c2f685ae6690fe15be40b3a5df9009
Pedro Benedetti <pbenedetti@itba.edu.ar>
2023-11-10 14:10:26 -0300

    Add files via upload

--- impparallel.py
+++ impparallel.py
@@ -7,17 +7,20 @@ import datetime
 
 adress = 0xCFE8
 
-def send_mark_biosemi(mark, adress):
-    parallel.setPortAddress(adress)
+def send_mark_biosemi(mark, adress,debug=0):
+    if debug == 0:
+        parallel.setPortAddress(adress)
 
-    time_start = datetime.datetime.now()
-    time_now = datetime.datetime.now()
-
-    while True:
-        parallel.setData(mark)
+        time_start = datetime.datetime.now()
         time_now = datetime.datetime.now()
-        print('B')
-        if (time_now - time_start).total_seconds() > 0.0015:
-            print((time_now - time_start).total_seconds())
-            parallel.setData(0)
-            break
+
+        while True:
+            parallel.setData(mark)
+            time_now = datetime.datetime.now()
+            print('B')
+            if (time_now - time_start).total_seconds() > 0.0015:
+                print((time_now - time_start).total_seconds())
+                parallel.setData(0)
+                break
+    elif debug == 1:
+        print('Sent a ' + mark + ' at ' + time_now)
